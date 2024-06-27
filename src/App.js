b85b3568570bac39626d3a85742ef2f4bb195951
unknown <Louis Pierre Andre>
2024-06-27 07:53:55 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -4,22 +4,17 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Welcom to website practice</h1>
+      <p>this is the first paragraphe</p>
+      <p>this is the first paragraphe</p>
+      <p>this is the first paragraphe</p>
+      <p>this is the first paragraphe</p>
     </div>
   );
 }
 
 export default App;
+
+
+// git init
+// git add.
